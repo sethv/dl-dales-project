92341b4f13b02cce4728711269cc94d2945a753d
Seth Vanderwilt <sethv@users.noreply.github.com>
2020-12-13 23:35:08 -0800

    Change backbone.py for resnet18. Should make options to work with multiple backbones

--- Deformable-DETR/models/backbone.py
+++ Deformable-DETR/models/backbone.py
@@ -79,7 +79,7 @@ class BackboneBase(nn.Module):
         else:
             return_layers = {'layer4': "0"}
             self.strides = [32]
-            self.num_channels = [2048]
+            self.num_channels = [512]
         self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
 
     def forward(self, tensor_list: NestedTensor):
@@ -103,7 +103,7 @@ class Backbone(BackboneBase):
         backbone = getattr(torchvision.models, name)(
             replace_stride_with_dilation=[False, False, dilation],
             pretrained=is_main_process(), norm_layer=norm_layer)
-        assert name not in ('resnet18', 'resnet34'), "number of channels are hard coded"
+        # assert name not in ('resnet18', 'resnet34'), "number of channels are hard coded"
         super().__init__(backbone, train_backbone, return_interm_layers)
         if dilation:
             self.strides[-1] = self.strides[-1] // 2
